14018eafaf34957c2018a7e2a194bd6508d10b3e
Angelene Lyu <angelalyu27@gmail.com>
2026-08-19 19:27:37 +1000

    45

--- Function 4: Transfer Funds/test_transfer_funds.py
+++ Function 4: Transfer Funds/test_transfer_funds.py
@@ -14,9 +14,17 @@ class TestTransferFunds:
                transfer_funds({"name": "Alice", "balance": 100000}, {"name": "KJK", "balance": 100000}, -1000)
 
     def test_fails_to_calculate_interest_when_rate_is_negative(self):
-               with pytest.raises(ValueError):
-                   transfer_funds({"name": "Alice", "balance": 50001}, {"name": "KJK", "balance": 100000}, 50001)
+            with pytest.raises(ValueError):
+                transfer_funds({"name": "Alice", "balance": 50001}, {"name": "KJK", "balance": 100000}, 50001)
 
     def test_fails_to_calculate_interest_when_rate_is_negative(self):
-               with pytest.raises(TypeError):
-                   transfer_funds({"name": "Alice", "balance": 100000}, {"name": "KJK", "balance": 100000}, "Hello")
+            with pytest.raises(TypeError):
+                transfer_funds({"name": "Alice", "balance": 100000}, {"name": "KJK", "balance": 100000}, "Hello")
+
+    def test_fails_to_calculate_interest_when_rate_is_negative(self):
+            with pytest.raises(ValueError):
+                transfer_funds({"name": "Alice", "balance": 50001}, {"name": "KJK", "balance": 100000}, 50001)
+
+    def test_fails_to_calculate_interest_when_rate_is_negative(self):
+            with pytest.raises(TypeError):
+                transfer_funds({"name": "Alice", "balance": 100000}, {"name": "KJK", "balance": 100000}, -1000)
